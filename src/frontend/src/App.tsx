b6bf383961bc568f7988fa085b695b8d54b35970
Alkamal30 <yurevich.vlad3005@gmail.com>
2025-11-17 16:18:16 +0300

    Add shrinking logic to onClick event

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,19 +1,44 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [value, setValue] = useState<string>("");
+  const [result, setResult] = useState<string>("");
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch(`https://localhost:5000/link/shrink?url=${value}`, {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          }
+        });
+
+      const res = await response.json()
+      setResult(`https://localhost:5000/${res}`)
+    } catch(err: any) {
+
+    } finally {
+
+    }
+  }
+
   return (
     <>
       <div>
         <h1>Shrink link in one click!</h1>
       </div>
       <div className="link-input">
-        <input className="link-input__field" type="text" placeholder="www.your-link.com"/>
-        <button className="link-input__action">
+        <input className="link-input__field" type="text" placeholder="www.your-link.com"
+          value={value} onChange={(e) => setValue(e.target.value)}/>
+        <button className="link-input__action" onClick={handleClick}>
           Shrink
         </button>
         <p className="link-input__tip">
           Enter your link and click button to shorten
         </p>
+        <a href={result}>{result}</a>
       </div>
     </>
   )
