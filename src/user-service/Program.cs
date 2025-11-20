daf7b9fad5ab8ac0c1dc71dddf77774b72083125
Alkamal30 <yurevich.vlad3005@gmail.com>
2025-11-20 16:10:16 +0300

    Add connection to DB

--- src/user-service/Program.cs
+++ src/user-service/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ShrinkLink.UserService.Domain.Data;
+using ShrinkLink.UserService.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<UserServiceContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserServiceContext>(provider => provider.GetRequiredService<UserServiceContext>());
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
