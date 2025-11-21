50d501d45cec31604e252a49c5edc666ebcf3de3
Alkamal30 <yurevich.vlad3005@gmail.com>
2025-11-21 15:18:40 +0300

    Rename UserController to UsersController

--- src/user-service/Presentation/Controllers/UsersController.cs
+++ src/user-service/Presentation/Controllers/UsersController.cs
@@ -4,7 +4,7 @@ namespace ShrinkLink.UserService.Presentation.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class UserController : ControllerBase
+public class UsersController : ControllerBase
 {
     [HttpGet]
     public IActionResult Get()
